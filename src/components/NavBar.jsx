4fb9e97066341b913d11a7d98e15dd6f59bc408e
karel-eaa <eaa25khan@students.eaaa.dk>
2026-05-19 18:54:31 +0200

    fix navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router";
+
 export function NavBar() {
     return (
         <div className="flex flex-col gap-12 md:gap-0 md:flex-row justify-between items-center px-[60px] pt-[40px]">
-            <img src="./nav.png" className="max-h-[100px] w-auto" />
+            <NavLink to={"/"}><img src="./nav.png" className="max-h-[100px] w-auto" /></NavLink>
             <div className="flex gap-[50px]">
-                <a className="bg-brand-red text-brand-light font-bowlby py-[9px] px-[18.5px] rounded-[15px] cursor-pointer text-[25px]">Buy tickets</a>
+                <NavLink to={"/tickets"} className="bg-brand-red text-brand-light font-bowlby py-[9px] px-[18.5px] rounded-[15px] cursor-pointer text-[25px]">Buy tickets</NavLink>
                 <img src="./menu-icon.svg" alt="" />
             </div>
         </div>
