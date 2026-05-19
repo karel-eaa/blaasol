236380112a1d843000f823d703d9db2c8edbd6ec
karel-eaa <eaa25khan@students.eaaa.dk>
2026-05-19 16:44:22 +0200

    fix footer for mobile

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -113,17 +113,17 @@ export function Home() {
                 <img className="h-[1021px] mx-auto mb-[-400px]" src="./footer-sun.png" alt="" />
             </div>
             <Section bgImg={"bg-footer.png"} className={"justify-end"} >
-                <div className="flex justify-between">
+                <div className="flex flex-col md:flex-row justify-between">
                     <div className="flex flex-col gap-[9px] px-[127px] py-[91px]">
                         <a href="" className="font-bowlby text-button-md text-[#0393CA]">Kontakt</a>
                         <a href="" className="font-bowlby text-button-md text-[#0393CA]">FAQ</a>
                         <a href="" className="font-bowlby text-button-md text-[#0393CA]">Partner</a>
                     </div>
                     <div className="flex items-end gap-[9px] px-[127px] py-[91px] gap-[46px]">
-                        <div className="flex items-center"><img className="h-[56px]" src="./instagram.png" alt="" /></div>
-                        <div className="flex items-center"><img className="h-[79px]" src="./facebook.png" alt="" /></div>
-                        <div className="flex items-center"><img className="h-[84px]" src="./youtube.png" alt="" /></div>
-                        <div className="flex items-center"><img className="h-[57px]" src="./spotify.png" alt="" /></div>
+                        <div className="flex items-center"><img className="h-6 md:h-12" src="./instagram.png" alt="" /></div>
+                        <div className="flex items-center"><img className="h-6 md:h-12" src="./facebook.png" alt="" /></div>
+                        <div className="flex items-center"><img className="h-6 md:h-12" src="./youtube.png" alt="" /></div>
+                        <div className="flex items-center"><img className="h-6 md:h-12" src="./spotify.png" alt="" /></div>
                     </div>
                 </div>
             </Section>
