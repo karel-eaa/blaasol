54d0e8e713c913e7f666f2c381f849dd76c6f7c5
karel-eaa <eaa25khan@students.eaaa.dk>
2026-05-21 16:44:34 +0200

    Volunteer page

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -2,28 +2,32 @@ import { useState } from "react";
 import { SectionDescription } from "./SectionDescription";
 import { SectionTitle } from "./SectionTitle";
 import { Stack } from "./Stack";
+import { ButtonLg } from "./ButtonLg";
 
-export function TicketCard({ titleText = "Standard ticket", descriptionText = "Vores vejledende digitale pris lige indtil d. 5. Juni 2026.", price = "880" }) {
+export function TicketCard({ titleText = "Standard ticket", descriptionText = "Vores vejledende digitale pris lige indtil d. 5. Juni 2026.", price = "880", subTitle, buttonText }) {
 
     let [counter, setCounter] = useState(0)
 
     return (<>
         <div className="bg-brand-light px-[40px] mx-[80px] mt-[100px] py-[20px] rounded-[20px] border-2 border-brand-red md:min-w-[800px] md:max-w-[800px]">
             <Stack className="items-start">
-                <SectionTitle text={titleText} className="" />
+                <SectionTitle text={titleText} className={`${subTitle ? "text-brand-red" : ""}`} />
+                {subTitle ? <SectionTitle text={subTitle} className="text-brand-dark" /> : ""}
                 <Stack className="items-start w-full">
-                    <SectionDescription text={descriptionText} className="text-base" />
+                    <SectionDescription text={descriptionText} className={subTitle ? "" : "text-base"} />
                     <div className="flex w-full justify-between">
                         <div className="flex flex-col">
-                            <SectionTitle className="text-brand-dark" text={price + " KR"} />
-                            <SectionDescription text="<b>+30 KR gebyr</b>" />
+                            <SectionTitle className="text-brand-dark" text={price + (subTitle ? "" : " KR")} />
+                            {subTitle ? null : <SectionDescription text="<b>+30 KR gebyr</b>" />}
                         </div>
-                        <div className="flex h-fit items-center">
-                            <div onClick={() => counter > 0 ? setCounter(counter - 1) : null }><SectionTitle className="text-brand-red border border-brand-red px-4 text-center rounded-[8px] cursor-pointer select-none" text="-" /></div>
-                            <SectionTitle className="text-brand-red px-4 text-center" text={counter} />
-                            <div onClick={() => setCounter(counter + 1)}><SectionTitle className="text-brand-red b-brand-dark border border-brand-red px-4 text-center rounded-[8px] cursor-pointer select-none" text="+" /></div>
-                        </div>
+                        {!subTitle &&
+                            <div className="flex h-fit items-center">
+                                <div onClick={() => counter > 0 ? setCounter(counter - 1) : null}><SectionTitle className="text-brand-red border border-brand-red px-4 text-center rounded-[8px] cursor-pointer select-none" text="-" /></div>
+                                <SectionTitle className="text-brand-red px-4 text-center" text={counter} />
+                                <div onClick={() => setCounter(counter + 1)}><SectionTitle className="text-brand-red b-brand-dark border border-brand-red px-4 text-center rounded-[8px] cursor-pointer select-none" text="+" /></div>
+                            </div>}
                     </div>
+                    {buttonText ? <ButtonLg text={buttonText} /> : null}
                 </Stack>
             </Stack>
         </div>
--- src/views/Volunteer.jsx
+++ src/views/Volunteer.jsx
@@ -1,5 +1,61 @@
+import { ButtonLg } from "../components/ButtonLg";
+import { Footer } from "../components/Footer";
+import { NavBar } from "../components/NavBar";
+import { Section } from "../components/Section";
+import { SectionDescription } from "../components/SectionDescription";
+import { SectionTitle } from "../components/SectionTitle";
+import { Stack } from "../components/Stack";
+import { TicketCard } from "../components/TicketCard";
+
 export function Volunteer() {
     return (<>
-        Volunteer
+        <Section classNameChildren={"items-start"} className={"min-h-fit"}>
+            <div className="w-full">
+                <NavBar blue={true} />
+            </div>
+        </Section>
+        <Section bgImg={"bg-bar.png"} className={"min-h-fit md:px-34 px-4 md:py-34 py-4 mt-8 bg-brand-blue"} gradient={false}>
+            <SectionTitle text="Become a volunteer at<br />BLA SOL!" className="text-brand-light text-center" />
+            <br />
+            <br />
+            <ButtonLg text="Start volunteering" />
+        </Section>
+        <Section bgImg={"bg-sun.png"} className={"min-h-fit"} bgPadding="md:inset-y-[30px] md:inset-x-[80px]">
+            <Stack className="pt-8" gap="gap-section-gap">
+                <div className="flex flex-col gap-8 items-center">
+                    <SectionTitle text="Get the experience of BLA SOL from the <br /> first line" />
+                    <SectionDescription className="mx-4 md:max-w-[700px]" text="Have you ever wondered what happens behind the scenes at a festival and what it’s like to be part of it? This is your chance! As a volunteer, you become part of the community that helps bring the festival to life and keeps the experience running smoothly." />
+                    <SectionDescription className="mx-4 md:max-w-[700px]" text="Have you ever wondered what happens behind the scenes at a festival and what it’s like to be part of it? This is your chance! As a volunteer, you become part of the community that helps bring the festival to life and keeps the experience running smoothly." />
+                </div>
+                <div className="flex flex-col gap-8">
+                    <SectionTitle text="What we expect? " />
+                    <SectionDescription className="mx-4 md:max-w-[700px]" text="- Your volunteer shift will last between 6 to 10 hours, depending on the role you apply for. In some cases, shifts may be split into multiple time slots. You will receive all relevant information once you have signed up." />
+                    <SectionDescription className="mx-4 md:max-w-[700px]" text="- Please make sure you are available during the times you provide. If your availability changes, let us know as soon as possible. Keep in mind that changes may not always be possible after the schedule has been finalized." />
+                </div>
+                <div className="flex flex-col gap-8">
+                    <SectionTitle text="What you get in return?" />
+                    <SectionDescription className="mx-4 md:max-w-[700px]" text="- Free drinks through the day.<br />
+                    - According to your working hours we provide 2 to 3 meals throughout the day.<br/>
+                    - Access to the festival before or after your shift." />
+                </div>
+                <ButtonLg text="Start volunteering" />
+            </Stack>
+        </Section>
+
+        <SectionTitle text="What we offer" className="md:mx-32 my-8" />
+        <Section bgImg={"bg-sun-center.png"} bgPadding="md:inset-y-[30px] md:inset-x-[80px]">
+            <Stack className="py-18">
+                <TicketCard buttonText="Sign Up" titleText="Before" subTitle={"10 hours of work"} descriptionText="The hours can be completed over 1 or 2 shifts before the day of the festival.<br/>- Help create the framework for an unforgettable experience when you assist with the setup of the festival grounds, decorations, and preparations.<br/>- Setup of booths and furnitureSetup of fences and tents<br/>- Setup of the stageDecorating the groundsSecurity leading up to the festival" price="Setup is from May 15 to June 5, 2026." />
+                <TicketCard buttonText="Sign Up" titleText="During" subTitle={"6 hours of work"} descriptionText="The hours must be completed in 1 continuous shift.Help ensure a great experience for guests, artists, and fellow volunteers on the day of the festival – with a smile, an overview, and hands ready to pitch in whenever needed.<br />- Ticket control<br />- Sales and information<br />- Security<br />- Bar<br />- Cleanup and cleaning" price="The festival is on June 6, 2026." />
+                <TicketCard buttonText="Sign Up" titleText="After" subTitle={"10 hours of work"} descriptionText="The hours can be completed over 1 or 2 shifts after the day of the festival.We want to leave Nordre Fælled in good condition, and you can help ensure that we can create a fantastic festival year after year.<br/>- Cleanup and cleaning<br/>- Teardown of fences and tents<br/>- Teardown of booths and furniture<br/>- Teardown of the stage<br/>- Security in the days after the festival" price="Teardown is from June 7 to June 30, 2026." />
+            </Stack>
+        </Section>
+        <Section className="min-h-fit">
+            <Stack className="mb-12">
+                <SectionTitle text="Still some questions?" className="text-brand-dark" />
+                <ButtonLg text="Contact US" />
+            </Stack>
+        </Section>
+        <Footer />
     </>)
 }
