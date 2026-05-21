6028203b49ffd411bb7f29279e2b4f3dd3bac910
karel-eaa <eaa25khan@students.eaaa.dk>
2026-05-21 17:02:12 +0200

    Update Volunteer.jsx

--- src/views/Volunteer.jsx
+++ src/views/Volunteer.jsx
@@ -42,7 +42,7 @@ export function Volunteer() {
             </Stack>
         </Section>
 
-        <SectionTitle text="What we offer" className="md:mx-32 my-8" />
+        <SectionTitle text="What we offer" className="text-center mt-32" />
         <Section bgImg={"bg-sun-center.png"} bgPadding="md:inset-y-[30px] md:inset-x-[80px]">
             <Stack className="py-18">
                 <TicketCard buttonText="Sign Up" titleText="Before" subTitle={"10 hours of work"} descriptionText="The hours can be completed over 1 or 2 shifts before the day of the festival.<br/>- Help create the framework for an unforgettable experience when you assist with the setup of the festival grounds, decorations, and preparations.<br/>- Setup of booths and furnitureSetup of fences and tents<br/>- Setup of the stageDecorating the groundsSecurity leading up to the festival" price="Setup is from May 15 to June 5, 2026." />
